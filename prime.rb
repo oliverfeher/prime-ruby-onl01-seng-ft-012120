a7f5169cc1590c28d6f84ebb6c6b6e703be8b74a
Oliver Feher <olcsee@gmail.com>
2020-01-24 08:34:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1 || number == 0 || number == 1
+    return false
+  elsif
+    (2..number - 1).each { |i|
+      if number % i == 0
+        return false
+      end 
+    }
+    end
+  true
+end
